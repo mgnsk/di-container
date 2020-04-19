26e72bcc7c2af3077bd2f8c530232d9b7d0e919c
Magnus Kokk <magnus@kokk.eu>
2020-04-20 01:10:58 +0300

    Uncomment example main

--- example/cmd/main.go
+++ example/cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/mgnsk/di-container/example"
+)
+
 func main() {
-	// s := example.InitMyService()
-	// fmt.Println(s.Greetings())
+	s := example.InitMyService()
+	fmt.Println(s.Greetings())
 }
